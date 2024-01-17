39e836f92580b0e8cc5b45f24a640c2d69d09606
raoneng26 <146713007+raoneng26@users.noreply.github.com>
2024-01-17 10:22:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,28 +7,39 @@ from selenium.webdriver.chrome.options import Options
 
 def run_selenium():
     # 设置Chrome选项
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # 使用无头模式
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options = Options()streamlit 
+    # chrome_options.add_argument("--headless")  # 使用无头模式
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
 
-    # 设置webdriver服务
-    webdriver_service = Service(ChromeDriverManager().install())
 
-    # 创建一个Selenium driver实例
-    driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    weibo_url = 'https://s.weibo.com/'
+    s = r"chrome-win64\chromedriver.exe"
+    s = Service(s) 
+    driver = webdriver.Chrome(service=s)
+    driver.get(weibo_url)
 
-    # 访问一个网页
-    driver.get("https://www.example.com")
 
+    # # 设置webdriver服务
+    # webdriver_service = Service(ChromeDriverManager().install())
+
+    # # 创建一个Selenium driver实例
+    # driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+    # # 访问一个网页
+    # driver.get("https://ww.example.com")
+#pl_homepage_search > div > div.nav > a.cur
     # 找到一个元素并获取其文本
-    element = driver.find_element(By.TAG_NAME, "h1")
-    element_text = element.text
+    driver.implicitly_wait(10)
+
+    element = driver.find_element(By.CSS_SELECTOR, "a.cur").text
+   
 
     # 关闭driver
     driver.quit()
 
-    return element_text
-
-# 在Streamlit应用中运行Selenium并显示结果
-st.write(run_selenium())
+    return element
+button=st.button("emmmm")
+if button:
+    # 在Streamlit应用中运行Selenium并显示结果
+    st.write(run_selenium())
