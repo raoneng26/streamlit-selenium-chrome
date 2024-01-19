836f36b0b98c56a8fca7b53970f8e068b4c2deb3
raoneng26 <146713007+raoneng26@users.noreply.github.com>
2024-01-19 21:59:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,54 @@
 import streamlit as st
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
+# Options for Chrome driver
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument("--no-sandbox")
+options.add_experimental_option("detach", True)
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+@st.cache_resource
+def get_driver():                     
+    s = r"chrome-win64\chromedriver.exe"
+    s = Service(s) 
+    return webdriver.Chrome(service=s)
 
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
+weibo_url = 'https://s.weibo.com/'
+driver = get_driver()
+                
+driver.get(weibo_url)
+time.sleep(30)
 
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
+data = []
+st.session_state.keyword="日本核污水"
+date ='2023-09-01-0:2023-11-08-23'
+for i in range(1, 50):
+    try:
+        w_url = f"https://s.weibo.com/weibo?q={st.session_state.keyword}&timescope=custom:{date}&Refer=g&sudaref=s.weibo.com&page={i}"
+        driver.get(w_url)
+        driver.implicitly_wait(10)
+        blogs = driver.find_elements(By.XPATH, "//div[@class='main-full']//div[@class='card-wrap']")
+        if blogs:
+            for blog in blogs:
+                publish_time = blog.find_element(By.CSS_SELECTOR, "div.from> a:nth-child(1)").text
+                up_name = blog.find_element(By.CSS_SELECTOR, ".name").text
+                try:
+                    blog_url = blog.find_element(By.CSS_SELECTOR, "div.from> a:nth-child(1)").get_attribute('href')
+                except:
+                    blog_url = 'NULL'
+                data.append({"发布者": up_name, "发布时间": publish_time, "博客url链接": blog_url})
+        else:
+            st.warning('爬取失败')
+    except:
+        st.warning('一共爬取了'+str(i-1)+'页,'+'页数' + str(i) + '不存在')
+        break
+driver.close()
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    # Options for Chrome driver
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
-    options.add_experimental_option("detach", True)
-    
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(options=options)
-
-    driver = get_driver()
-    driver.get("http://example.com")
-
-    st.code(driver.page_source)
-
+df = pd.DataFrame(data)
