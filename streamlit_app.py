b3b85cd9f770ee20edd5c748ca35335bcd699f72
raoneng26 <146713007+raoneng26@users.noreply.github.com>
2024-01-20 10:13:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,31 @@
+"""Scrapes quotes from the given url.
+
+The url is 'https://quotes.toscrape.com/'.
+"""
+
 import streamlit as st
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+import pandas as pd
 
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-size=1920x1080')
+chrome_options.add_argument('--disable-gpu')
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
-driver = get_driver()
-driver.get('http://example.com')
+url = 'https://quotes.toscrape.com/'
+driver.get(url)
 
-st.code(driver.page_source)
+quotes = driver.find_elements('xpath', '//span[@class="text"]')
+
+data = []
+for q in quotes:
+    data.append(q.text)
+    
+driver.quit()
+
+df = pd.DataFrame(data, columns=['Quotes'])
+st.dataframe(df)
